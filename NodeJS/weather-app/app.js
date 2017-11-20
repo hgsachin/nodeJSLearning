8ebf006936e7a0d8b06bef614b8699d18f3bd262
hgsachin <hgsachin@gmail.com>
2017-11-20 22:20:24 +0530

    weather app

--- NodeJS/weather-app/app.js
+++ NodeJS/weather-app/app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
             .options({
@@ -12,20 +12,11 @@ const argv = yargs
             })
             .help()
             .argv;
-        
-const addr = encodeURIComponent(argv.address);
 
-request({
-    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${addr}`,
-    json: true
-}, (error, response, body) => {
-    if(error){
-        console.log('Unable to connect to Google server');
-    } else if(body.status === 'ZERO_RESULTS'){
-        console.log('Unable to find the address');
-    } else if(body.status === 'OK') {
-        console.log(`Address : ${body.results[0].formatted_address}`);
-        console.log('Lattitude : ', body.results[0].geometry.location.lat);
-        console.log('Longitude : ', body.results[0].geometry.location.lng);
+geocode.geocodeAddress(argv.address, (errorMsg, results) => {
+    if(errorMsg){
+        console.log(errorMsg);
+    } else {
+        console.log(JSON.stringify(results, undefined, 4));
     }
 });
